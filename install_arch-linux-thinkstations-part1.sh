a369930791badaeab4bbc4cecca330464144988d
Claudio Lepin <claudio.lepin@uchile.cl>
2020-11-19 11:01:20 -0300

    Fix

--- install_arch-linux-thinkstations-part1.sh
+++ install_arch-linux-thinkstations-part1.sh
@@ -5,11 +5,3 @@ mkfs.fat -F32 /dev/sda1
 mkfs.ext4 -F /dev/sda2
 mkfs.ext4 -F /dev/sda3
 mkfs.ext4 -F /dev/sda4
-mount /dev/sda2 /mnt
-mount /dev/sda1 /mnt/boot/efi
-mount /dev/sda4 /mnt/home
-vim /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware
-genfstab -U /mnt >> /mnt/etc/fstab
-cat /mnt/etc/fstab
-arch-chroot /mnt
--- install_arch-linux-thinkstations-part2.sh
+++ install_arch-linux-thinkstations-part2.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-ln -sf /usr/share/zoneinfo/America/Santiago /etc/localtime
-hwclock --systohc
-pacman -S vim intel-ucode efibootmgr grub netctl iw dhcpcd xf86-video-intel lightdm lightdm-gtk-greeter xorg-server xorg-xinit xorg-xinput xorg-xhost sudo udisks2 mtpfs gvfs-mtp gvfs-gphoto2 git openssh base-devel jq htop nmap nbtscan ettercap tcpdump jp2a imagemagick xdg-utils sxiv entr nitrogen xclip groff zathura zathura-pdf-mupdf zathura-ps zathura-cb zathura-djvu notify-osd libnotify picom dmenu neofetch bspwm sxhkd
-sed 's/#es_CL.UTF-8/es_CL.UTF-8/g' /etc/locale.gen
-locale-gen
-echo 'LANG=es_CL.UTF-8' > /etc/locale.conf
-echo 'magi-system' > /etc/hostname
-echo -e '127.0.0.1 localhost\n::1       localhost\n127.0.0.1 magi-system.localdomain magi-system' > /etc/hosts
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Windows Boot Manager"
-grub-mkconfig -o /boot/grub/grub.cfg
-systemctl enable lightdm
-echo -e "sxhdc &\nexec bspwm" > ~/.xinitrc
-useradd -m -g users -G wheel -s /bin/bash clepin
-passwd clepin
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-git clone https://aur.archlinux.org/yay.git && /yay/makepkg -si && rm -rf yay/
-#yay -S nvidia-340xx polybar
+#mount /dev/sda2 /mnt
+#mount /dev/sda1 /mnt/boot/efi
+#mount /dev/sda4 /mnt/home
+
+vim /etc/pacman.d/mirrorlist
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab
+#arch-chroot /mnt
