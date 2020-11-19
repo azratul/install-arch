15447990ff3032798a4d646e4731e9652cf875cd
Claudio Lepin <claudio.lepin@uchile.cl>
2020-11-19 11:26:41 -0300

    Fix

--- install_arch-linux-thinkstations-part1.sh
+++ install_arch-linux-thinkstations-part1.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-mkdir -p /mnt/boot/efi /mnt/home
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 -F /dev/sda2
 mkfs.ext4 -F /dev/sda3
 mkfs.ext4 -F /dev/sda4
+mount /dev/sda2 /mnt
+mkdir -p /mnt/boot/efi /mnt/home
+mount /dev/sda1 /mnt/boot/efi
+mount /dev/sda4 /mnt/home
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+#arch-chroot /mnt
