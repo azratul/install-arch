28f56ae17d5f14314450b8ae743a02ffa91830b4
Claudio Lepin <azratul@gmail.com>
2023-12-22 12:33:45 -0300

    New branch

--- part2.sh
+++ part2.sh
@@ -2,7 +2,7 @@
 
 ln -sf /usr/share/zoneinfo/America/Santiago /etc/localtime
 hwclock --systohc
-pacman -S vim netctl iw dhcpcd sudo neofetch udisks2 mtpfs gvfs-mtp gvfs-gphoto2 git openssh base-devel jq nmap nbtscan ettercap tcpdump jp2a imagemagick sxiv nitrogen groff zathura zathura-pdf-mupdf zathura-ps zathura-cb zathura-djvu dialog bash-completion lsd wpa_supplicant sddm neofetch amd-ucode efibootmgr grub kitty nvidia-dkms nvidia-utils hyprland qt5-wayland qt5ct libva wlroots seatd linux linux-headers
+pacman -S vim netctl iw dhcpcd sudo neofetch udisks2 mtpfs gvfs-mtp gvfs-gphoto2 git openssh base-devel jq nmap nbtscan ettercap tcpdump jp2a imagemagick sxiv nitrogen groff zathura zathura-pdf-mupdf zathura-ps zathura-cb zathura-djvu dialog bash-completion lsd wpa_supplicant sddm neofetch amd-ucode efibootmgr grub kitty nvidia-dkms nvidia-utils hyprland qt5-wayland qt5ct libva wlroots seatd linux linux-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack wofi waybar hyprpaper rtkit pavucontrol
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 clocksource=hpet nvidia_drm.modeset=1"/' /etc/default/grub
 sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
 echo 'options nvidia-drm modeset=1' >> /etc/modprobe.d/nvidia.conf
@@ -14,6 +14,7 @@ echo 'magi-system' > /etc/hostname
 echo -e '127.0.0.1 localhost magi-system.localdomain magi-system\n::1       localhost magi-system.localdomain magi-system' > /etc/hosts
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Arch Linux"
 grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable rtkit-daemon.service
 systemctl enable sddm
 useradd -m -g users -G wheel -s /bin/bash clepin
 echo "Password Clepin"
--- part3.sh
+++ part3.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+systemctl --user enable pipewire.socket
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://aur.archlinux.org/yay.git
 cd ./yay/
 makepkg -si
 cd ..
 rm -rf yay/
-yay -S tdrop polybar
 echo "setxkbmap -layout latam -variant deadtilde" > .xprofile
 sudo pacman -S firefox-developer-edition kubectl mplayer docker blender gimp inkscape
 git clone https://github.com/azratul/dotfiles
