0ffd030758bf39d73a6cfd666ed0ac4662b15501
Claudio Lepín <azratul@gmail.com>
2023-12-27 10:05:21 -0300

    New branch

--- part3.sh
+++ part3.sh
@@ -6,5 +6,5 @@ cd ./yay/
 makepkg -si
 cd ..
 rm -rf yay/
-sudo pacman -S firefox-developer-edition kubectl mplayer docker blender gimp inkscape dolphin ttf-fira-code noto-fonts-cjk otf-font-awesome
-yay -S gotop wlr-randr
+sudo pacman -S firefox-developer-edition kubectl mplayer docker blender gimp inkscape dolphin ttf-fira-code noto-fonts-cjk otf-font-awesome ttf-liberation nodejs npm
+yay -S gotop wlr-randr fastfetch
