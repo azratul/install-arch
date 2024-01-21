8272b7a9aa1d640b21d61f72a7a9a86e882d961a
Claudio Lepín <azratul@gmail.com>
2024-01-21 15:47:26 -0300

    fix

--- part3.sh
+++ part3.sh
@@ -7,7 +7,9 @@ makepkg -si
 cd ..
 rm -rf yay/
 sudo pacman -S firefox-developer-edition kubectl mplayer docker blender gimp inkscape dolphin ttf-fira-code noto-fonts-cjk otf-font-awesome ttf-liberation nodejs npm
-yay -S gotop wlr-randr fastfetch
+yay -S gotop wlr-randr neofetch qownnotes it87-dkms-git
+sudo modprobe it87 force_id=0x8686
+# APPEND TO /etc/default/grub GRUB_CMDLINE_LINUX_DEFAULT="acpi_enforce_resources=lax"
 
 # Steam
 sudo pacman -S lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader ttf-liberation lib32-systemd wine-staging lib32-giflib lib32-libpng lib32-gnutls lib32-mpg123 openal lib32-openal lib32-v4l-utils lib32-libpulse alsa-plugins lib32-alsa-plugins lib32-alsa-lib lib32-libjpeg-turbo lib32-sqlite lib32-libxcomposite lib32-libxinerama lib32-opencl-icd-loader libxslt lib32-libxslt lib32-libva lib32-gtk3 lib32-gst-plugins-base-libs meson lib32-nvidia-utils vulkan-tools lutris
